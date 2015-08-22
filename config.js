fa0a4338d9f094ba68d270b109f03566f99dd544
Petar Mitchev <petar@imeleon.com>
2015-08-22 15:52:19 +0300

    Pool api changed

--- config.js
+++ config.js
@@ -1,10 +1,8 @@
-var api = "http://pool-107.democats.org:8083";
+var api = "http://pool-api.democats.org:8083";
 
-var poolHost = "pool-107.democats.org";
+var poolHost = "pool-api.democats.org";
 
 var irc = "irc.freenode.net/#democats";
-
-var email = "";
 
 var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
 
