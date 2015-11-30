0d53ee46441c2c68999f3e5b9e04804f43687bff
Petar Mitchev <petar@imeleon.com>
2015-11-30 09:47:10 +0200

    Blockexplorer links changed

--- config.js
+++ config.js
@@ -11,9 +11,11 @@ var cryptonatorWidget = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR"];
 
 var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
 
-var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
+//var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
+var blockchainExplorer = "/?hash={id}#blockchain_block";
 
-var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
+//var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
+var transactionExplorer = "/?hash={id}#blockchain_transaction";
 
 var themeCss = "themes/ease-way-light-theme.css";
 
